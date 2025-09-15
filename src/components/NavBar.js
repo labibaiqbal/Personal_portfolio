ff0af7e15aa96f58d37bbeec42675cb4016f0583
labibaiqbal <66547474+labibaiqbal@users.noreply.github.com>
2025-09-15 12:43:47 -0700

    changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon3 from "../assets/img/icons8-github-500.svg";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import resumePDF from "../assets/LabibaIqbal_NewGrad_resume.pdf";
 
 export const NavBar = () => {
@@ -28,62 +28,58 @@ export const NavBar = () => {
   };
 
   return (
-    <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link
-                href="#home"
-                className={
-                  activeLink === "home" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("home")}
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("skills")}
-              >
-                Experiences
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === "projects"
-                    ? "active navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("projects")}
-              >
-                Projects
-              </Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/labiba-iqbal-06627b219/">
-                  <img src={navIcon1} alt="linkedin" />
-                </a>
-                <a href="https://github.com/labibaiqbal">
-                  <img src={navIcon3} alt="github" />
-                </a>
-              </div>
-              <a href={resumePDF} target="_blank" rel="noopener noreferrer">
-                <button className="vvd">
-                  <span>My Resume</span>
-                </button>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Experiences
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/labiba-iqbal-06627b219/">
+                <img src={navIcon1} alt="linkedin" />
               </a>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Router>
+              <a href="https://github.com/labibaiqbal">
+                <img src={navIcon3} alt="github" />
+              </a>
+            </div>
+            <a href={resumePDF} target="_blank" rel="noopener noreferrer">
+              <button className="vvd">
+                <span>My Resume</span>
+              </button>
+            </a>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
